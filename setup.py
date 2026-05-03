e318737f44dcb34b00183eb068f140bc939cb3fd
User <user@example.com>
2026-05-03 23:05:47 +0300

    Use underscore package name for Bench install

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="company-restriction",
+    name="company_restriction",
     version="1.0.0",
     author="Your Name",
     author_email="your@email.com",
